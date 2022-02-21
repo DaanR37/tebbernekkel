f818284d1196d01ace69189b603e7f170523e122
dukemai <duc.mai@bonniernews.se>
2022-02-21 23:34:43 +0100

    now workign slide

--- src/components/Featured/Cards/Card.js
+++ src/components/Featured/Cards/Card.js
@@ -1,19 +1,11 @@
-import { CircularProgress } from '@mui/material';
-import './card.scss';
+import "./card.scss";
 // import Box from '@mui/material/Box';
 // import { LinearProgress } from '@mui/material';
 
 export default function Card({ active, title, progress }) {
-    return (
-        <div className={`card ${active ? 'active' : ''}`}>
-
-            <CircularProgress
-                variant="determinate"
-                value={progress}
-                size={100}
-                thickness={3}
-            />
-            <h3>{title}</h3>
-        </div>
-    );
+  return (
+    <div className={`slide-item__card card ${active ? "active" : ""}`}> 
+      <h3>{title}</h3>
+    </div>
+  );
 }
