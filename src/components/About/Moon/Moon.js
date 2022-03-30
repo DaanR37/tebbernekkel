95c26dbfe22bc8c4131280993b1fb68c831015a5
Daan Roelofs <daanroelofs599@gmail.com>
2022-03-30 12:42:56 +0200

    deleting unused files

--- src/components/About/Moon/Moon.js
+++ src/components/About/Moon/Moon.js
@@ -1,65 +1,21 @@
-import React from 'react'
-import { useRef } from 'react';
-import { useLoader } from '@react-three/fiber';
-import { TextureLoader } from 'three';
-import { OrbitControls, Stars } from "@react-three/drei";
-import * as THREE from "three";
-
-import MoonColorMap from "../../../assets/textures/moon_color_map_kopie.jpg";
-import MoonNormalMap from "../../../assets/textures/moon_normal_map_kopie.jpg";
+import { Stars } from "@react-three/drei";
 
 export default function Moon(props) {
-
-    const [colorMap, normalMap] = useLoader
-        (TextureLoader,
-            [MoonColorMap, MoonNormalMap]
-        );
-
-    const moonRef = useRef();
-
     return (
-            <>
-                <pointLight
-                    color="#f6f3ea"
-                    position={[-3.9, -1, 5]}
-                    // position={[15, 0, 5]}
-                    intensity={1.21}
-                />
-                <Stars
-                    radius={300}
-                    depth={60}
-                    count={800}
-                    factor={9.1}
-                    saturation={0}
-                    fade={true}
-                />
-                {/* <mesh ref={moonRef} position={[1.1, -0.1, 2.5]} rotation={[-50, -15, 75]}>
-                <sphereGeometry args={[1.005, 32, 32]} />
-                <meshPhongMaterial
-                    map={normalMap}
-                    // opacity={0.4}
-                    // depthWrite={true}
-                    // transparent={true}
-                    side={THREE.DoubleSide}
-                />
-                <meshStandardMaterial
-                    map={colorMap}
-                    colorMap={colorMap}
-                    metalness={0.9}
-                    roughness={0.9}
-                    // opacity={0.4}
-                    depthWrite={true}
-                    transparent={true}
-                />
-                <OrbitControls
-                    enableZoom={false}
-                    enablePan={false}
-                    enableRotate={true}
-                    zoomSpeed={0.6}
-                    panSpeed={0.5}
-                    rotateSpeed={0.4}
-                />
-            </mesh> */}
-            </>
+        <>
+            <pointLight
+                color="#f6f3ea"
+                position={[-3.9, -1, 5]}
+                intensity={1.21}
+            />
+            <Stars
+                radius={100}
+                depth={40}
+                count={1500}
+                factor={4}
+                saturation={4}
+                fade={true}
+            />
+        </>
     )
 }
