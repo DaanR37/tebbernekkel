718a880f156285eecc8ad1040d64cc9477e70ecf
Daan Roelofs <daanroelofs599@gmail.com>
2023-10-06 11:48:10 +0200

    order change videos

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -5,56 +5,56 @@ import HeaderTransparent from '../Headertransparent/HeaderTransparent';
 import "./featured.scss";
 
 export default function Featured() {
-    const videoIndex = ["two", "one", "three", "four", "six", "five", "eight", "seven", 
+  const videoIndex = ["two", "one", "three", "four", "six", "five", "eight", "seven",
     "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen"];
-    const [portfolioData, setPortfolioData] = useState([]);
+  const [portfolioData, setPortfolioData] = useState([]);
 
-    useEffect(() => {
-        // fetch("http://localhost:3001/featured")
-        fetch("https://api.tebbernekkel.nl/featured")
-            .then(response => response.json())
-            .then(portfolioData => {
-                setPortfolioData(portfolioData)
-            }
-            ).catch(error => {
-                console.log(error.message)
-            }
+  useEffect(() => {
+    // fetch("http://localhost:3001/featured")
+    fetch("https://api.tebbernekkel.nl/featured")
+      .then(response => response.json())
+      .then(portfolioData => {
+        setPortfolioData(portfolioData)
+      }
+      ).catch(error => {
+        console.log(error.message)
+      }
+      )
+  }, []);
+  return (
+    <Animatedpage>
+      <section id="featured-container">
+        <div className="header-transparent-featured">
+          <HeaderTransparent />
+        </div>
+        <div id="grid-wrapper-featured">
+          {portfolioData.map((video, index) => {
+            return (
+              <div className={videoIndex[index]} key={index}>
+                <Link
+                  to="/embeddedplayerfeatured"
+                  state={{
+                    link: `${video.uri}`,
+                    title: `${video.name}`,
+                    description: `${video.description}`,
+                  }}
+                >
+                  <div
+                    style={{
+                      backgroundImage: `url(${video.pictures.sizes[5].link})`,
+                      height: "100%",
+                      width: "100%"
+                    }}>
+                    <div className="ho-featured">
+                      <span aria-hidden="true">{video.name}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
             )
-    }, []);
-    return (
-        <Animatedpage>
-            <section id="featured-container">
-                <div className="header-transparent-featured">
-                    <HeaderTransparent />
-                </div>
-                <div id="grid-wrapper-featured">
-                    {portfolioData.map((video, index) => {
-                        return (
-                            <div className={videoIndex[index]} key={index}>
-                                <Link
-                                    to="/embeddedplayerfeatured"
-                                    state={{
-                                        link: `${video.uri}`,
-                                        title: `${video.name}`,
-                                        description: `${video.description}`,
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            backgroundImage: `url(${video.pictures.sizes[5].link})`,
-                                            height: "100%",
-                                            width: "100%"
-                                        }}>
-                                        <div className="ho-featured">
-                                            <span aria-hidden="true">{video.name}</span>
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })}
-                </div>
-            </section>
-        </Animatedpage>
-    );
+          })}
+        </div>
+      </section>
+    </Animatedpage>
+  );
 }
